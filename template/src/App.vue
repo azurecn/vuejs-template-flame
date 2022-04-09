b35f687f133d95e9cc0685e8e6ba6a5279a06ec4
AzureCN <colorazure@163.com>
2022-04-09 12:06:26 +0800

    Update App.vue

--- template/src/App.vue
+++ template/src/App.vue
@@ -1,11 +1,13 @@
 <template>
   <div id="app">
+    {{#router}}
     <router-view/>
+    {{/router}}
   </div>
 </template>
 
 <script>
 export default {
-  name: 'app'
+  name: 'app'{{#router}}{{/router}}
 }
 </script>
